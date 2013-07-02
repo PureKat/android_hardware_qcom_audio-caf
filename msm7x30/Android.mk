e7c43c829f6a5dc7b01de9bf49a849acb0789025
Nicholas Flintham <nick@flinny.org>
2013-07-02 11:38:21 +0100

    msm7x30 - Use kernel headers
    Change-Id: I850512c85cabe01e1885cefb3c4956d977076f72

--- msm7x30/Android.mk
+++ msm7x30/Android.mk
@@ -77,6 +77,8 @@ LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
 LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
 
